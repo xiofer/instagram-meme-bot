ceff4591605641cbe916f87773414d3418fe5050
xiofer <iamironmannn957@gmail.com>
2026-06-16 22:31:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import requests
+import os
+
+app = FastAPI()
+
+IG_BUSINESS_ID = os.getenv("IG_BUSINESS_ID")
+PAGE_ACCESS_TOKEN = os.getenv("PAGE_ACCESS_TOKEN")
+
+
+class ReelRequest(BaseModel):
+    video_url: str
+    caption: str
+
+
+@app.get("/")
+def root():
+    return {"status": "working"}
+
+
 @app.post("/publish-reel")
 def publish_reel(data: ReelRequest):
 
