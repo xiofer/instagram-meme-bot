978bd65541ae45bcc1c62e73aeff1f0d76700340
xiofer <iamironmannn957@gmail.com>
2026-06-16 22:08:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import requests
+import os
 
 app = FastAPI()
 
+IG_BUSINESS_ID = os.getenv("IG_BUSINESS_ID")
+PAGE_ACCESS_TOKEN = os.getenv("PAGE_ACCESS_TOKEN")
+
 class ReelRequest(BaseModel):
-    image_url: str
+    video_url: str
     caption: str
 
 @app.get("/")
 def root():
     return {"status": "working"}
 
-@app.post("/create-reel")
-def create_reel(data: ReelRequest):
-    return {
-        "status": "success",
-        "image_url": data.image_url,
-        "caption": data.caption
-    }
+@app.post("/publish-reel")
+def publish_reel(data: ReelRequest):
+
+    create_url = f"https://graph.facebook.com/v25.0/{IG_BUSINESS_ID}/media"
+
+    create_response = requests.post(
+        create_url,
+        data={
+            "media_type": "REELS",
+            "video_url": data.video_url,
+            "caption": data.caption,
+            "access_token": PAGE_ACCESS_TOKEN
+        }
+    ).json()
+
+    return create_response
