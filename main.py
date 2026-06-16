d981b8c18fe53eaaad575d820b5a96583b41763d
xiofer <iamironmannn957@gmail.com>
2026-06-16 18:30:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class ReelRequest(BaseModel):
+image_url: str
+caption: str
+
 @app.get("/")
 def root():
-  
-  return {"status": "working"}
+return {"status": "working"}
+
+@app.post("/create-reel")
+def create_reel(data: ReelRequest):
+return {
+"status": "success",
+"image_url": data.image_url,
+"caption": data.caption
+}
